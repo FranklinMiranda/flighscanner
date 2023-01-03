e2590df45fbcc317b9279c79ae11eb2bc154aa81
FranklinMiranda <fdm61344@gmail.com>
2023-01-03 11:05:23 -0500

    CM

--- flightscanner/src/Components/Data.js
+++ flightscanner/src/Components/Data.js
@@ -1,27 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 // API endpoint URL https://jsonplaceholder.typicode.com/posts/
 
 const Data = () => {
-  const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
-      setLoading(true);
 
-      const response = await Axios.get('https://jsonplaceholder.typicode.com/posts/');
+      const response = await axios.get('https://the-cocktail-db.p.rapidapi.com/search.php');
+        console.log(response)
+      //setData(response.data);
 
-      setData(response.data);
-
-      setLoading(true);
     };
 
     loadData();
-  }, []);
+  });
 
-  return <div className="Data">{loading ? <h4>Loading...</h4> : data.map((item) => <h4>{item.title}</h4>)}</div>;
+  return <div className="Data">{data.map((item) => <h4>{item.title}</h4>)}</div>;
 };
 
 export default Data
+
