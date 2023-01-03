19373898c00e2d6a32cbc9cb643ab174f2f96883
FranklinMiranda <fdm61344@gmail.com>
2023-01-03 10:21:25 -0500

    CM

--- flightscanner/src/Components/App.js
+++ flightscanner/src/Components/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
-    <div className='App'>
-        <h1> App </h1>
+  return (
+    <div className="App">
+      <h1> App </h1>
     </div>
-}
+  );
+};
 
-export default App
+export default App;
